ee9f9d7ff855d2106b61564d00261569cd85f4f4
AndersonShen <shen.andersonshen@gmail.com>
2022-01-16 22:58:44 +0800

    產品基礎排版

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -75,7 +75,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 html,
 body {
   height: 100%;
--- src/components/products.vue
+++ src/components/products.vue
@@ -1,14 +1,39 @@
 <template>
   <div class="container">
-    <div class="btn btn-primary"></div>
+    <div class="row justify-content-center align-items-center">
+      <template v-for="item in products" :key="item.id">
+        <h2>{{ item.title }}</h2>
+        <div class="col-4">
+          <p>產品分類：{{ item.category }}</p>
+          <p>產品說明：{{ item.content }}</p>
+          <p>產品描述：{{ item.description }}</p>
+          <p>產品ID：{{ item.id }}</p>
+          <p>原價：{{ item.origin_price }}</p>
+          <p>特價：{{ item.price }}</p>
+          <p>剩餘數量：{{ item.num }} {{ item.unit }}</p>
+        </div>
+        <div class="col-6">
+          <img class="img-fluid" :src="item.imageUrl" alt="" />
+          <template v-for="(pic, idx) in item.imagesUrl" :key="idx">
+            <img class="img-fluid w-25" :src="pic" alt="" />
+          </template>
+        </div>
+      </template>
+    </div>
   </div>
 </template>
+
 <script>
 import axios from "axios";
 const url = "https://vue3-course-api.hexschool.io/v2"; // 請加入站點
 const path = "ashen"; // 請加入個人 API path
 
 export default {
+  data() {
+    return {
+      products: [],
+    };
+  },
   methods: {
     getProducts() {
       // #5 取得後台產品列表
@@ -16,6 +41,7 @@ export default {
         .get(`${url}/api/${path}/admin/products`)
         .then((res) => {
           console.log(res.data);
+          this.products = res.data.products;
         })
         .catch((err) => {
           console.log(err.response.data.message);
