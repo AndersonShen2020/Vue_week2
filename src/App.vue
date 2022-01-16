ac5648184907dd26c36e4e0071d6a3571f8e958b
AndersonShen <shen.andersonshen@gmail.com>
2022-01-16 17:05:03 +0800

    追加 vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,43 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-  <login></login>
+  <!-- <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav> -->
+  <router-view />
 </template>
 
 <script>
 // import HelloWorld from "./components/HelloWorld.vue";
-import Login from "./components/Login.vue";
-
-export default {
-  name: "App",
-  components: {
-    // HelloWorld,
-    Login,
-  },
-};
+// import Login from "./components/Login.vue";
+// export default {
+//   name: "App",
+//   components: {
+//     // HelloWorld,
+//     Login,
+//   },
+// };
 </script>
 
 <style lang="scss">
 @import "~bootstrap/scss/bootstrap";
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+// nav {
+//   padding: 30px;
+
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
 </style>
