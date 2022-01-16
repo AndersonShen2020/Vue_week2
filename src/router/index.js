9f810bbe1c12be574f9d5c9c4227f33fe50de784
AndersonShen <shen.andersonshen@gmail.com>
2022-01-17 02:13:56 +0800

    調整 router 設定

--- src/router/index.js
+++ src/router/index.js
@@ -13,19 +13,13 @@ const routes = [
     name: "products",
     component: products,
   },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
 ];
 
+const history = createWebHistory(process.env.BASE_URL);
+// const history = createWebHashHistory();
+
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history,
   routes,
 });
 
